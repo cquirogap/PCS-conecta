7ae2a2f7dbebf0fc2f68916cb1d779135cba2c0b
conectapcsservidor <conectapcsservidor@gmail.com>
2022-06-10 14:15:14 +0000

    Backup Base de Datos

--- pcs/pcs/settings.py
+++ pcs/pcs/settings.py
@@ -136,7 +136,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gestiondocumentaletet@gmail.com'
-EMAIL_HOST_PASSWORD = 'ztwsxsdxzbpwappm'
+EMAIL_HOST_PASSWORD = 'wtkyxnjypbdwqbxx'
 
 CRONJOBS = [
     ('*/2 * * * *', 'interlocutorc.views.tarea')
