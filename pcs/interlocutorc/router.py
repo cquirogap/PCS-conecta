be9ace098ca4e92612dab16c9d80a5018f777b07
conectapcsservidor <conectapcsservidor@gmail.com>
2023-01-31 23:59:55 +0000

    Backup Base de Datos

--- pcs/interlocutorc/router.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from interlocutorc.views import ApiPrueba
-
-router_posts=DefaultRouter()
-
-router_posts.register(prefix='post',basename='post',viewset=ApiPrueba)
