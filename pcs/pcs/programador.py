05ab7712a3b050778ba065a38050a3e283109cb0
conectapcsservidor <conectapcsservidor@gmail.com>
2025-07-15 10:37:08 -0500

    Backup Base de Datos

--- pcs/pcs/programador.py
+++ pcs/pcs/programador.py
@@ -6,5 +6,5 @@ def ejecutar_tarea():
     os.system('source /home/pcsenv/bin/activate && cd /home/pcs-conecta/pcs && python manage.py tarea')
 
 sched = BlockingScheduler()
-sched.add_job(ejecutar_tarea, 'cron', hour=14, minute=0)  # Ejecutar todos los días a las 14:00
+sched.add_job(ejecutar_tarea, 'cron', hour=14, minute=0)  # Ejecutar todos los dias a las 14:00
 sched.start()
--- pcs/pcs/settings.py
+++ pcs/pcs/settings.py
@@ -150,7 +150,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'conectaportalweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'usgmfmngtrkgvwxb'
+EMAIL_HOST_PASSWORD = 'aqyhafchutqwxjku'
 
 
 
