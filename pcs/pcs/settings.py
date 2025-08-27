8d3a911dffcd7056b343c2eb24316e52ae39baf6
root <root@159.178.153.160.host.secureserver.net>
2025-08-27 21:30:35 +0000

    Backup Base de Datos

--- pcs/pcs/settings.py
+++ pcs/pcs/settings.py
@@ -149,8 +149,8 @@ LOGIN_URL = '/login/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'conectaportalweb2@gmail.com'
-EMAIL_HOST_PASSWORD = 'wgne noum hjso piwz'
+EMAIL_HOST_USER = 'conectaportalweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'aqyhafchutqwxjku'
 
 
 
