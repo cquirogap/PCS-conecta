5c980a2feb041c2c122bcac86a0ad753b7c8010e
Naren Rios Doza <narenriosdoza@gmail.com>
2026-02-12 11:36:01 -0500

    Ip_servidor de calidad y pruebas

--- pcs/configuracion/views.py
+++ pcs/configuracion/views.py
@@ -57,11 +57,11 @@ PAGINADOR = 9999999999999999999999999999999
 
 # ************* IP PRODUCCION ******************
 #IP_SAP = 'https://192.168.1.2:50000/b1s/v1/'
-#IP_SERVIDOR = 'https://192.155.95.186'
+#IP_SERVIDOR = 'https://160.153.178.159'
 
 # ************* IP CALIDAD ******************
 IP_SAP = 'https://172.16.100.20:50000/b1s/v1/'
-IP_SERVIDOR = 'https://160.153.178.159'
+IP_SERVIDOR = 'https://192.155.95.186'
 
 
 def link_callback(uri, rel):
